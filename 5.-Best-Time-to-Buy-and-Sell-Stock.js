dcb0775ae9021b06798511025103045780158481
Victor <64720348+Gitpiff@users.noreply.github.com>
2022-07-04 14:20:23 -0500

    Update 5.-Best-Time-to-Buy-and-Sell-Stock.js

--- 5.-Best-Time-to-Buy-and-Sell-Stock.js
+++ 5.-Best-Time-to-Buy-and-Sell-Stock.js
@@ -4,10 +4,47 @@
 
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+// const maxProfit = (prices) => {
+//     //buy first, however do not buy at the highest price, buy low sell high
+//     //do not sell stock at a loos, or when given an array in descending order
+//     //two pointers, one for when we buy, one for when we sell
+//     let buy = prices[0];
+//     let profit = 0;
+
+//     //start iterating at index 1, index 0 was taken by buy var
+//    for (let i = 1; i < prices.length; i ++) {
+//        //if buy price is greater than the current price, reassing the var
+//         if (buy > prices[i]) {
+//             buy = prices[i]
+//         }
+//         //else calculate the max profit between the subtraction of the current price minus the buy var, and the current value in the profit var
+//         else {
+//             profit = Math.max(prices[i] - buy, profit)
+//         }
+//    }
+
+//    return profit;
+// }
+
+//Solution 2
 const maxProfit = (prices) => {
-    //two pointers, one for when we buy, one for when we sell
+    //two ppointers
     let buy = 0;
-    let sell = 0;
+    let sell = 1;
+    let max_profit = 0;
+    while (sell < prices.length) {
+        if (prices[buy] < prices[sell]) {
+            let profit = prices[sell] - prices[buy];
+
+            max_profit = Math.max(max_profit, profit);
+        } else {
+            buy = sell;
+        }
+
+        sell ++;
+    }
+
+    return max_profit;
 }
 
 console.log(maxProfit([7,1,5,3,6,4]))
